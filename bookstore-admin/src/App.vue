2c9b5b829b63ca972e8d53ba6f536b9535529325
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2020-01-30 00:11:38 +0100

    init bo

--- bookstore-admin/src/App.vue
+++ bookstore-admin/src/App.vue
@@ -1,28 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <Admin></Admin>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { Admin } from "vuetify-admin";
 
 export default {
-  name: "app",
+  name: "App",
   components: {
-    HelloWorld
+    Admin
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
