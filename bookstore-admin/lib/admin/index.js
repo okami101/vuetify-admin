bcfe2509ccee68b3d5f79a126d66100effb1b086
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2020-01-30 21:07:35 +0100

    refactor admin init

--- bookstore-admin/lib/admin/index.js
+++ bookstore-admin/lib/admin/index.js
@@ -1,12 +1,27 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Vuex from "vuex";
+import vuetify from "./plugins/vuetify";
+
 import Admin from "./components/admin";
 
 const components = [Admin];
 
-export default {
-  install(Vue, options) {
-    components.forEach(component => {
-      console.log(component.name);
-      Vue.component(component.name, component);
-    });
-  }
-};
+components.forEach(component => {
+  console.log(component.name);
+  Vue.component(component.name, component);
+});
+
+Vue.use(VueRouter);
+Vue.use(Vuex);
+
+const router = new VueRouter({});
+const store = new Vuex.Store({});
+
+export default App =>
+  new Vue({
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
+  });
--- bookstore-admin/src/main.js
+++ bookstore-admin/src/main.js
@@ -1,21 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
-import VueRouter from "vue-router";
-import Vuex from "vuex";
-import vuetify from "./plugins/vuetify";
-import Admin from "vuetify-admin";
+import AdminVue from "vuetify-admin";
 
 Vue.config.productionTip = false;
-Vue.use(VueRouter);
-Vue.use(Vuex);
-Vue.use(Admin);
 
-const router = new VueRouter({});
-const store = new Vuex.Store({});
-
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
-}).$mount("#app");
+AdminVue(App).$mount("#app");
