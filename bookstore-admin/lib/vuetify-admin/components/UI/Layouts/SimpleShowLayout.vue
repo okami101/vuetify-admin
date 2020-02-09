08d56e550a888afd47f7d11a404d97413829d958
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2020-02-09 14:03:36 +0100

    dev show

--- bookstore-admin/lib/vuetify-admin/components/UI/Layouts/SimpleShowLayout.vue
+++ bookstore-admin/lib/vuetify-admin/components/UI/Layouts/SimpleShowLayout.vue
@@ -1,16 +1,40 @@
 <template>
-  <v-list dense>
-    <v-list-item>
-      <v-list-item-content>Calories:</v-list-item-content>
-      <v-list-item-content class="align-end">{{
-        item.calories
-      }}</v-list-item-content>
-    </v-list-item>
-  </v-list>
+  <v-row>
+    <v-col sm="auto">
+      <v-list dense>
+        <v-list-item v-for="field in fields" :key="field.value">
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ field.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+          <v-list-item-content>
+            <slot
+              :name="field.value"
+              v-bind="{ item: resource, value: resource[field.value] }"
+            >
+              {{ resource[field.value] }}
+            </slot>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 export default {
-  name: "SimpleShowLayout"
+  name: "SimpleShowLayout",
+  props: {
+    fields: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      resource: this.$route.meta.model
+    };
+  }
 };
 </script>
