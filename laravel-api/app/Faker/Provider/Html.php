7a11cc8c114bb5f9db2874c4bf230639e245cd61
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2020-02-09 16:47:08 +0100

    dev tab layout

---
+++ laravel-api/app/Faker/Provider/Html.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Faker\Provider;
+
+use Faker\Provider\Base;
+
+class Html extends Base
+{
+    /**
+     * Generate rich body.
+     *
+     * @param Generator|\Faker\UniqueGenerator $faker
+     *
+     * @return string
+     */
+    public function htmlParagraphs($min = 1, $max = 5, $sentences = 10): string
+    {
+        $html = '';
+
+        /*
+         *  Generate many paragraphs
+         */
+        for ($k = 0; $k < $this->generator->numberBetween($min, $max); $k++) {
+            $paragraph = $this->generator->paragraph($sentences);
+            $html .= "<p>$paragraph</p>";
+        }
+
+        return $html;
+    }
+}
