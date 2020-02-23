fb1f672ac3db7e836cf3478592c5d6a4bf4e61f9
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2020-02-23 11:24:27 +0100

    simplify components imports

--- admin/vuetify-admin/lib/vuetify-admin/index.js
+++ admin/vuetify-admin/lib/vuetify-admin/index.js
@@ -1,88 +1,12 @@
-import {
-  Admin,
-  Resource,
-  Field,
-  Input,
-  List,
-  Show,
-  Create,
-  Edit
-} from "./components/Core";
-
-import {
-  Datagrid,
-  SimpleShow,
-  TabbedShow,
-  Form,
-  FormSave,
-  SimpleForm,
-  TabbedForm,
-  AsideContent,
-  ListButton,
-  CreateButton,
-  ShowButton,
-  EditButton,
-  DeleteButton,
-  SaveButton,
-  ExportButton,
-  CloneButton,
-  TextField,
-  DateField,
-  NumberField,
-  RatingField,
-  RichTextField,
-  BooleanField,
-  TextInput,
-  DateInput,
-  NumberInput,
-  PasswordInput,
-  RatingInput,
-  BooleanInput
-} from "./components/UI";
-
-const components = [
-  Admin,
-  Resource,
-  Field,
-  Input,
-  List,
-  Show,
-  Create,
-  Edit,
-  Datagrid,
-  Form,
-  FormSave,
-  SimpleShow,
-  TabbedShow,
-  SimpleForm,
-  TabbedForm,
-  AsideContent,
-  ListButton,
-  CreateButton,
-  ShowButton,
-  EditButton,
-  DeleteButton,
-  SaveButton,
-  ExportButton,
-  CloneButton,
-  TextField,
-  DateField,
-  NumberField,
-  RatingField,
-  RichTextField,
-  BooleanField,
-  TextInput,
-  DateInput,
-  NumberInput,
-  PasswordInput,
-  RatingInput,
-  BooleanInput
-];
+import * as core from "./components/Core";
+import * as ui from "./components/UI";
 
 export default {
   install(Vue) {
-    components.forEach(component => {
-      Vue.component(`Va${component.name}`, component);
+    [core, ui].forEach(c => {
+      Object.keys(c).forEach(name => {
+        Vue.component(`Va${name}`, c[name]);
+      });
     });
   }
 };
