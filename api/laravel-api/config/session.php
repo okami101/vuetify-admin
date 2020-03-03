82f9cb62a9e595152c12e637646bc8b357ddf222
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2020-03-03 23:36:07 +0100

    init airlock

--- api/laravel-api/config/session.php
+++ api/laravel-api/config/session.php
@@ -153,7 +153,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', '.localhost'),
+    'domain' => env('SESSION_DOMAIN', null),
 
     /*
     |--------------------------------------------------------------------------
