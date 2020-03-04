63af3583c0ee09da3c862ad6fb045f08233a7756
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2020-03-04 23:31:46 +0100

    bump laravel skeleton

--- api/laravel-api/config/app.php
+++ api/laravel-api/config/app.php
@@ -82,7 +82,7 @@ return [
     |
     */
 
-    'locale' => 'fr',
+    'locale' => env('APP_LOCALE', 'en'),
 
     /*
     |--------------------------------------------------------------------------
@@ -209,6 +209,7 @@ return [
         'File' => Illuminate\Support\Facades\File::class,
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
+        'Http' => Illuminate\Support\Facades\Http::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
--- api/laravel-api/config/session.php
+++ api/laravel-api/config/session.php
@@ -194,6 +194,6 @@ return [
     |
     */
 
-    'same_site' => 'none',
+    'same_site' => 'lax',
 
 ];
