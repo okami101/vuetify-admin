9a6f25886a28291393a94386d5380546fe641125
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2020-03-08 21:17:48 +0100

    draggable inputs array

--- admin/lib/vuetify-admin/components/UI/Inputs/ArrayInput.vue
+++ admin/lib/vuetify-admin/components/UI/Inputs/ArrayInput.vue
@@ -1,7 +1,13 @@
 <template>
   <va-input v-bind="$props" class="va-array-input">
-    <ul v-if="input.length">
-      <li v-for="(item, i) in input" :key="i">
+    <draggable
+      v-if="input.length"
+      :value="input"
+      draggable=".item"
+      handle=".handle"
+      @input="update"
+    >
+      <div v-for="(item, i) in input" :key="i" class="item">
         <div>
           <component
             v-for="childInput in getInputs"
@@ -12,24 +18,39 @@
             v-model="input[i][childInput.source]"
           ></component>
         </div>
-        <v-btn outlined color="red" @click.stop="remove(i)">
-          <v-icon small>mdi-minus-circle-outline</v-icon>
-          <span class="ml-2">{{ $t("va.actions.remove") }}</span>
-        </v-btn>
-      </li>
-    </ul>
-    <v-btn outlined color="green" @click.stop="add">
-      <v-icon small>mdi-plus-circle-outline</v-icon>
-      <span class="ml-2">{{ $t("va.actions.add") }}</span>
-    </v-btn>
+        <div class="d-flex">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn outlined icon text v-on="on" class="handle">
+                <v-icon small>mdi-cursor-move</v-icon>
+              </v-btn>
+            </template>
+            <span>{{ $t("va.actions.move") }}</span>
+          </v-tooltip>
+          <v-spacer></v-spacer>
+          <v-btn outlined color="red" @click.stop="remove(i)">
+            <v-icon small>mdi-minus-circle-outline</v-icon>
+            <span class="ml-2">{{ $t("va.actions.remove") }}</span>
+          </v-btn>
+        </div>
+      </div>
+      <v-btn slot="footer" outlined color="green" @click.stop="add">
+        <v-icon small>mdi-plus-circle-outline</v-icon>
+        <span class="ml-2">{{ $t("va.actions.add") }}</span>
+      </v-btn>
+    </draggable>
   </va-input>
 </template>
 
 <script>
 import Input from "vuetify-admin/mixins/input";
+import draggable from "vuedraggable";
 
 export default {
   name: "ArrayInput",
+  components: {
+    draggable
+  },
   mixins: [Input],
   props: {
     inputs: {
@@ -80,21 +101,11 @@ export default {
   margin-bottom: 0.5rem;
 }
 
-ul {
-  list-style: none;
-  padding: 0;
-  margin: 0;
-
-  li {
-    display: flex;
-    flex-direction: column;
-    padding-bottom: 2rem;
-    border-bottom: solid 1px var(--v-primary-lighten5);
-    margin-bottom: 2rem;
-
-    .v-btn {
-      margin-left: auto;
-    }
-  }
+.item {
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 2rem;
+  border-bottom: solid 1px var(--v-primary-lighten5);
+  margin-bottom: 2rem;
 }
 </style>
