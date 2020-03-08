a816d52280518dc636f852083a4497478f90583b
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2020-03-08 23:21:25 +0100

    refactor

--- admin/lib/vuetify-admin/index.js
+++ admin/lib/vuetify-admin/index.js
@@ -1,8 +1,11 @@
 import * as core from "./components/Core";
 import * as ui from "./components/UI";
+import PortalVue from "portal-vue";
 
 export default {
   install(Vue) {
+    Vue.use(PortalVue);
+
     [core, ui].forEach(c => {
       Object.keys(c).forEach(name => {
         Vue.component(`Va${name}`, c[name]);
