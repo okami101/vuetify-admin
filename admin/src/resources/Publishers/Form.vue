7cf383296c7bfdecdf1fa58a1f98f29d6966c68c
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2020-03-14 19:24:52 +0100

    enhance publisher layouts

--- admin/src/resources/Publishers/Form.vue
+++ admin/src/resources/Publishers/Form.vue
@@ -1,42 +1,77 @@
 <template>
   <va-form :item="item" :saving.sync="saving">
-    <v-card>
-      <v-card-text>
-        <v-row>
-          <v-col sm="3">
-            <va-file-input source="logo">
-              <va-image-field
-                source="logo"
-                preview="thumbnails.small"
-                clearable
-              ></va-image-field>
-            </va-file-input>
-            <va-text-input source="name"></va-text-input>
-            <va-select-input source="type" enum></va-select-input>
-            <va-text-input source="founder"></va-text-input>
-            <va-text-input source="description" multiline></va-text-input>
-            <va-text-input source="headquarter"></va-text-input>
-            <va-text-input source="url"></va-text-input>
-            <va-text-input source="email"></va-text-input>
-            <va-boolean-input source="active"></va-boolean-input>
-            <va-text-input source="address.street"></va-text-input>
-            <va-text-input source="address.postcode"></va-text-input>
-            <va-text-input source="address.city"></va-text-input>
-            <va-file-input source="local" multiple>
-              <va-image-field
-                source="local"
-                preview="thumbnails.small"
-                clearable
-              ></va-image-field>
-            </va-file-input>
-            <va-date-input source="opening_date" format="long"></va-date-input>
-          </v-col>
-        </v-row>
-      </v-card-text>
-      <v-toolbar flat color="grey lighten-4">
-        <va-save-button :saving="saving"></va-save-button>
-      </v-toolbar>
-    </v-card>
+    <v-row>
+      <v-col sm="6" offset="3">
+        <v-card>
+          <v-card-text>
+            <v-row>
+              <v-col lg="6">
+                <va-file-input source="logo">
+                  <va-image-field
+                    source="logo"
+                    preview="thumbnails.small"
+                    clearable
+                  ></va-image-field>
+                </va-file-input>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col sm="6">
+                <va-text-input source="name"></va-text-input>
+              </v-col>
+              <v-col sm="6">
+                <va-select-input source="type" enum></va-select-input>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col sm="6">
+                <va-text-input source="founder"></va-text-input>
+              </v-col>
+              <v-col sm="6">
+                <va-text-input source="headquarter"></va-text-input>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col sm="12">
+                <va-text-input source="description" multiline></va-text-input>
+                <va-file-input source="local" multiple>
+                  <va-image-field
+                    source="local"
+                    preview="thumbnails.small"
+                    clearable
+                  ></va-image-field>
+                </va-file-input>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col sm="6">
+                <va-text-input source="url"></va-text-input>
+              </v-col>
+              <v-col sm="6">
+                <va-text-input source="email"></va-text-input>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col sm="6">
+                <va-text-input source="address.street"></va-text-input>
+                <va-text-input source="address.postcode"></va-text-input>
+                <va-text-input source="address.city"></va-text-input>
+              </v-col>
+              <v-col sm="6">
+                <va-date-input
+                  source="opening_date"
+                  format="long"
+                ></va-date-input>
+                <va-boolean-input source="active"></va-boolean-input>
+              </v-col>
+            </v-row>
+          </v-card-text>
+          <v-toolbar flat color="grey lighten-4">
+            <va-save-button :saving="saving"></va-save-button>
+          </v-toolbar>
+        </v-card>
+      </v-col>
+    </v-row>
   </va-form>
 </template>
 
--- admin/src/resources/Publishers/Show.vue
+++ admin/src/resources/Publishers/Show.vue
@@ -1,50 +1,64 @@
 <template>
   <va-show>
-    <v-card>
-      <v-card-text>
-        <va-field source="logo">
-          <va-image-field
-            source="logo"
-            src="thumbnails.large"
-            preview="thumbnails.medium"
-          ></va-image-field>
-        </va-field>
-        <va-field source="name"></va-field>
-        <va-field
-          source="type"
-          type="select"
-          :options="{ enum: true }"
-        ></va-field>
-        <va-field source="founder"></va-field>
-        <va-field
-          source="description"
-          :options="{ multiline: true }"
-        ></va-field>
-        <va-field source="headquarter"></va-field>
-        <va-field source="url" type="url"></va-field>
-        <va-field source="email" type="email"></va-field>
-        <va-field source="active" type="boolean"></va-field>
-        <va-field source="address" :label="$t('address')" v-slot="{ value }">
-          {{ value.street }}, {{ value.postcode }} {{ value.city }}
-        </va-field>
-        <va-field
-          source="opening_date"
-          type="date"
-          :options="{ format: 'long' }"
-        ></va-field>
-        <va-field
-          source="books_count"
-          type="number"
-          v-can="['admin']"
-        ></va-field>
-        <va-field source="local">
-          <va-image-field
-            source="local"
-            src="thumbnails.large"
-            preview="thumbnails.medium"
-          ></va-image-field>
-        </va-field>
-      </v-card-text>
-    </v-card>
+    <v-row>
+      <v-col sm="6" offset="3">
+        <v-card>
+          <v-card-text>
+            <va-field source="logo">
+              <va-image-field
+                source="logo"
+                src="thumbnails.large"
+                preview="thumbnails.medium"
+              ></va-image-field>
+            </va-field>
+            <va-field source="name"></va-field>
+            <va-field
+              source="type"
+              type="select"
+              :options="{ enum: true }"
+            ></va-field>
+            <va-field source="founder"></va-field>
+            <va-field source="headquarter"></va-field>
+            <va-field
+              source="description"
+              :options="{ multiline: true }"
+            ></va-field>
+            <v-row>
+              <v-col lg="6">
+                <va-field source="url" type="url"></va-field>
+                <va-field source="email" type="email"></va-field>
+                <va-field source="active" type="boolean"></va-field>
+              </v-col>
+              <v-col lg="6">
+                <va-field
+                  source="address"
+                  :label="$t('address')"
+                  v-slot="{ value }"
+                >
+                  {{ value.street }}, {{ value.postcode }} {{ value.city }}
+                </va-field>
+                <va-field
+                  source="opening_date"
+                  type="date"
+                  :options="{ format: 'long' }"
+                ></va-field>
+                <va-field
+                  source="books_count"
+                  type="number"
+                  v-can="['admin']"
+                ></va-field>
+              </v-col>
+            </v-row>
+            <va-field source="local">
+              <va-image-field
+                source="local"
+                src="thumbnails.large"
+                preview="thumbnails.medium"
+              ></va-image-field>
+            </va-field>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </va-show>
 </template>
