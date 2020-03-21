5c90d99ffb67124f4add74427ee6f673a2890ba0
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2020-03-21 19:42:01 +0100

    up deps

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 7.2.2 on 2020-03-21 10:36:18.
+ * Generated for Laravel 7.2.2 on 2020-03-21 18:41:33.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
