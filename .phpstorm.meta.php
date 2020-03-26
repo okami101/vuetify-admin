0f44699be70dab3f610afff9532f0cdae1fa48e6
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2020-03-26 20:52:00 +0100

    init generators code

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-03-26 14:07:46.
+    * Generated on 2020-03-26 19:42:15.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 7.3.0 on 2020-03-26 14:07:45.
+ * Generated for Laravel 7.3.0 on 2020-03-26 19:42:14.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
