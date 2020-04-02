8705d4fb70bfc82740be5e9504b1ba4e69bf0dd2
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2020-04-02 19:31:46 +0200

    up deps

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-04-01 19:04:17.
+    * Generated on 2020-04-02 19:10:22.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 7.4.0 on 2020-04-01 19:04:17.
+ * Generated for Laravel 7.4.0 on 2020-04-02 19:10:21.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
