70a017fd2154573158ed1a988c6f0d825396c2cd
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2020-04-05 21:16:02 +0200

    up deps

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-04-04 18:08:56.
+    * Generated on 2020-04-05 21:15:35.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
