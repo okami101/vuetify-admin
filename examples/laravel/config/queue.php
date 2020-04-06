05da65134e0e58ad19247e2aa36e8c2c4058a918
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2020-04-06 14:09:06 +0200

    change default

--- examples/laravel/config/queue.php
+++ examples/laravel/config/queue.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_CONNECTION', 'redis'),
+    'default' => env('QUEUE_CONNECTION', 'sync'),
 
     /*
     |--------------------------------------------------------------------------
