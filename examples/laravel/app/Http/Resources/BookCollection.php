fb3cdfe9cab82630b92150c476c06bf885f3567c
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2020-04-27 23:11:59 +0200

    remove unuseful complex fields

---
+++ examples/laravel/app/Http/Resources/BookCollection.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Vtec\Crud\Http\Resources\BaseResource;
+
+class Book extends BaseResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        $attributes = parent::toArray($request);
+
+        $attributes['publisher'] = Publisher::make($this->whenLoaded('publisher'));
+        $attributes['authors'] = Author::collection($this->whenLoaded('authors'));
+        $attributes['reviews'] = Review::collection($this->whenLoaded('reviews'));
+
+        $attributes += [
+            'links' => [
+                'self' => route('books.show', $this->id),
+            ],
+        ];
+
+        return $attributes;
+    }
+}
