5b30493e0cba8c7820f6c92f7358b56ce9791b17
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2020-05-28 21:32:01 +0200

    remove console prod

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,6 @@ module.exports = {
     node: true,
   },
   extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-  },
   parserOptions: {
     parser: "babel-eslint",
   },
